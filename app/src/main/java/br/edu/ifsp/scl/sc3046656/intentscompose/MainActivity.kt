8493e52cc811495797de52d8be8b6eaf1e24bd5d
C4rloos0 <c.alexandreecorrea@gmail.com>
2026-09-16 18:28:05 -0300

    feat: implement MainActivity

--- app/src/main/java/br/edu/ifsp/scl/sc3046656/intentscompose/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3046656/intentscompose/MainActivity.kt
@@ -7,10 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import br.edu.ifsp.scl.sc3046656.intentscompose.navigation.MainNavHost
 import br.edu.ifsp.scl.sc3046656.intentscompose.ui.theme.IntentsComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +17,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navHostController = rememberNavController()
             IntentsComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainNavHost(
+                        navHostController = navHostController,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IntentsComposeTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/br/edu/ifsp/scl/sc3046656/intentscompose/composable/HomeScreen.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3046656/intentscompose/composable/HomeScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,7 +25,7 @@ fun HomeScreen(
     onAddWordClick: (String) -> Unit,
 
 ){
-    var stringAtual by remember {mutableStateOf("")}
+    var stringAtual by rememberSaveable {mutableStateOf("")}
 
     LaunchedEffect(palavraRecebida) {
         if(palavraRecebida.isNotEmpty()){
